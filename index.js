194e6bfb4197fc88607c6d96d1ce59cf5dd57747
Joyce Njoroge <joycewambuku33@gmail.com>
2023-03-17 10:19:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1=62;
+let num2=1;
+const multiply=num1*num2;
+
+let num3=24;
+let num4=6;
+const mod=num3/num4;
+
+const random=1;
+
+const arr = [1, 20, 3];
+const max = Math.max(...arr);
